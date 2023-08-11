4f7458f1404fbb3f7a366ac8c08362c11aac3cf6
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-08-11 08:13:10 +0530

    Routing & Navigate started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DirectivesComponent } from './directives/directives.component';
@@ -15,6 +17,23 @@ import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
 import { CounterService } from './counter.service';
 //custom service providers added in here AccountService
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
+  { path: 'servers', component: ServersComponent },
+ 
+
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,13 +44,22 @@ import { CounterService } from './counter.service';
     AccountComponent,
     NewAccountComponent,
     ActiveUsersComponent,
-    InactiveUsersComponent
+    InactiveUsersComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [AccountService,LoggingService,CounterService],
+  providers: [AccountService,LoggingService,CounterService,ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
